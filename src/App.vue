7d7553127387bde57c9da56a446a852ea5da66ae
Vermouth <761123274@qq.com>
2019-08-04 10:41:01 +0800

    项目搭建

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <TabBer/>
+    <loading/>
+  </div>
+</template>
+<script>
+import TabBer from "common/tabBer"
+import http from "utils/http.js"
+import Loading from "lib/loading/index.js"
+export default {
+  name:"App",
+  components:{
+    TabBer,
+    Loading
+  },
+   created(){
+     http("get","/home/index/getRecommendShow?cityAdd=&page=1&version=6.0.1&referer=2").then((data)=>{
+       console.log(data)
+     })
+   }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import home from "./home"
+import list from "./list"
+import order from "./order"
+import mine from "./mine"
+Vue.use(VueRouter)
+
+export default new VueRouter({
+    // 设置重定项
+    routes:[
+        {
+            path:"/",
+            redirect:"/home"
+        },
+        home,
+        list,
+        order,
+        mine
+    ]
+})
